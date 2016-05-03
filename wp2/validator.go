10a173bf0314452f2fc1c428d8ac4b67f0a227e0
nguyenthethinh <nguyenthethinh@gmail.com>
2016-05-03 16:42:17 -0700

    Validate from server side

---
+++ wp2/validator.go
@@ -0,0 +1,10 @@
+package finalproject
+
+import (
+	"regexp"
+)
+
+func validateEmail(email string) bool {
+	Re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	return Re.MatchString(email)
+}
